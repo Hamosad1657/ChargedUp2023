63d940df8f56b3d8bd478f81d341cdd40e59b33b
Asriel-dmr <jonjonberg10@gmail.com>
2023-02-25 15:34:16 +0200

    Berg: Changed kPaths commands

--- src/main/java/frc/robot/commands/swerve/autonomous/SwervePathConstants.java
+++ src/main/java/frc/robot/commands/swerve/autonomous/SwervePathConstants.java
@@ -9,6 +9,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.arm.ArmSubsystem;
 import frc.robot.subsystems.arm.ArmConstants.ArmState;
@@ -72,8 +73,8 @@ public final class SwervePathConstants {
 		SwervePathConstants.kPathCommandsMap.put("ArmMiddle", arm.setStateCommand(ArmState.kMid));
 		SwervePathConstants.kPathCommandsMap.put("ArmLow", arm.setStateCommand(ArmState.kLowFront));
 		SwervePathConstants.kPathCommandsMap.put("ToggleGrabber", grabber.toggleGrabberSolenoidCommand());
-		SwervePathConstants.kPaths.put("Path One", new SequentialCommandGroup(null));
-		SwervePathConstants.kPaths.put("Path Two", new SequentialCommandGroup(null));
-		SwervePathConstants.kPaths.put("Path Three", new SequentialCommandGroup(null));
+		SwervePathConstants.kPaths.put("Path One", new SequentialCommandGroup(new InstantCommand()));
+		SwervePathConstants.kPaths.put("Path Two", new SequentialCommandGroup(new InstantCommand()));
+		SwervePathConstants.kPaths.put("Path Three", new SequentialCommandGroup(new InstantCommand()));
 	}
 }
