172bd8f834401084efec5410c50f6ff12d5b9553
Asriel-dmr <jonjonberg10@gmail.com>
2023-02-25 22:48:56 +0200

    Berg: Removed outdated hashmap

--- src/main/java/frc/robot/commands/arm/ArmActuatorConstants.java
+++ src/main/java/frc/robot/commands/arm/ArmActuatorConstants.java
@@ -15,10 +15,4 @@ public class ArmActuatorConstants {
 
 	public static final double kAngleThreshold = 1;
 	public static final double kFrontAngle = 0;
-
-	public static final HashMap<Integer, List<Pose2d>> tagIDtoGrid = new HashMap<Integer, List<Pose2d>>();
-	static {
-		// TODO: Add each pose for each ID.
-		tagIDtoGrid.put(null, null);
-	}
 }
