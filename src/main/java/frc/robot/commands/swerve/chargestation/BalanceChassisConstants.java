555b46421e2dbd8a688c73eaa207b3e612bfaf55
שקד <shakedshibo@gmail.com>
2023-03-13 18:51:38 +0200

    Sufi: Tweaked balancing PID

--- src/main/java/frc/robot/commands/swerve/chargestation/BalanceChassisConstants.java
+++ src/main/java/frc/robot/commands/swerve/chargestation/BalanceChassisConstants.java
@@ -7,6 +7,6 @@ public class BalanceChassisConstants {
 	public static final double kGroundAngle = 0.0;
 	public static final double kTolerance = 2.5;
 	public static final double kDriveSpeedMPS = 0.7;
-	public static final PIDGains kPIDGains = new PIDGains(0.0575, 0, 0.025);
+	public static final PIDGains kPIDGains = new PIDGains(0.055, 0, 0.025);
 	public static final double kWaitingTimeSec = 0.35;
 }
