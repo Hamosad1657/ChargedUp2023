0ba36f2c873196784f0de487e7d82c489f6d4e45
שקד <shakedshibo@gmail.com>
2023-03-13 16:15:40 +0200

    Updated flipTurretCommand

--- src/main/java/frc/robot/subsystems/turret/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/turret/TurretSubsystem.java
@@ -125,14 +125,14 @@ public class TurretSubsystem extends SubsystemBase {
 
 	public Command flipTurretCommand() {
 		double setpoint;
-		double currentAngle = this.getCurrentAngle();
-		// Within a 10 degree tolerance to 270
-		if (Math.abs(currentAngle) < TurretConstants.kFrontRotationSetpoint + 10
-				&& Math.abs(currentAngle) > TurretConstants.kFrontRotationSetpoint - 10) {
+
+		// Within a 10 degree tolerance to 90
+		if (Math.abs(TurretConstants.kFrontRotationSetpoint - this.getCurrentAngle()) < 10.0) {
 			setpoint = TurretConstants.kBackRotationSetpoint;
 		} else {
 			setpoint = TurretConstants.kFrontRotationSetpoint;
 		}
+
 		return new InstantCommand(() -> this.setSetpoint(setpoint))
 				.andThen(new WaitUntilCommand(() -> this.isAtSetpoint(TurretConstants.kAutoRotationTolerance)));
 	}
