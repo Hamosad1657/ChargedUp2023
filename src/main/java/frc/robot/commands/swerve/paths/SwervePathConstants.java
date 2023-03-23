481ea7339ac838a5983405c1ccb2ec536eeedb49
ShakedMev <shakedshibo@gmail.com>
2023-03-23 12:31:57 +0200

    Sufi: Changed auto home timout

--- src/main/java/frc/robot/commands/swerve/paths/SwervePathConstants.java
+++ src/main/java/frc/robot/commands/swerve/paths/SwervePathConstants.java
@@ -109,7 +109,7 @@ public final class SwervePathConstants {
 				turret.flipTurretCommand().deadlineWith(arm.getToStateCommand(ArmState.kHalfClosed, true)));
 
 		// Arm
-		SwervePathConstants.kPathCommandsMap.put("HomeArm", arm.autoHomeCommand().withTimeout(3.0));
+		SwervePathConstants.kPathCommandsMap.put("HomeArm", arm.autoHomeCommand().withTimeout(6.0));
 		SwervePathConstants.kPathCommandsMap.put("ArmHigh", arm.getToStateCommand(ArmState.kHigh, true));
 		SwervePathConstants.kPathCommandsMap.put("ArmMid", arm.getToStateCommand(ArmState.kMid, true));
 		SwervePathConstants.kPathCommandsMap.put("ArmLowCone", arm.getToStateCommand(ArmState.kLowCone, true));
