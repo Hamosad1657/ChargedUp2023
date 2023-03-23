89e2e2c23fe2cfeb86b632ac9265987268beaafe
ShakedMev <shakedshibo@gmail.com>
2023-03-23 07:53:09 +0200

    Shaked: Added almost home arm state to high cone and cube

--- src/main/java/frc/robot/commands/swerve/paths/SwervePathConstants.java
+++ src/main/java/frc/robot/commands/swerve/paths/SwervePathConstants.java
@@ -118,6 +118,7 @@ public final class SwervePathConstants {
 				arm.getToStateLengthFirstCommand(ArmState.kHalfClosed, true).withTimeout(4.0)
 						.handleInterrupt(() -> arm.autoHomeCommand().schedule()));
 		SwervePathConstants.kPathCommandsMap.put("RetractArm", arm.retractCommand().withTimeout(4.0));
+		SwervePathConstants.kPathCommandsMap.put("AlmostHome", arm.getToStateCommand(ArmState.kAlmostHome, true));
 
 		// Grabber
 		SwervePathConstants.kPathCommandsMap.put("CollectGamePiece", grabber.collectCommand());
